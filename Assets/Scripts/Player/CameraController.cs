778b9c202e8b38047805b2ca7550197217b4c03e
Thomas Wiltshire <thomas.wiltshire14@icloud.com>
2018-03-13 16:27:09 +1100

    First Commit.

---
+++ Assets/Scripts/Player/CameraController.cs
@@ -0,0 +1,86 @@
+﻿// using, etc
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//--------------------------------------------------------------------------------------
+// CameraController object. Inheriting from MonoBehaviour. The main class for camera movement
+// behaviour.
+//--------------------------------------------------------------------------------------
+public class CameraController : MonoBehaviour
+{
+    // PUBLIC VALUES //
+    //--------------------------------------------------------------------------------------
+    // public floats for the min and max of the camera movement
+    public float m_fMinimumX = -60.0f;
+    public float m_fMaximumX = 60.0f;
+    public float m_fMinimumY = -360.0f;
+    public float m_fMaximumY = 360.0f;
+
+    // public floats for the camera sensitivity
+    public float m_fSensitivityX = 15.0f;
+    public float m_fSensitivityY = 15.0f;
+
+    //
+    public Vector3 offset;
+
+    // public camera value for the camera compoent
+    public Camera m_cCamera;
+    //--------------------------------------------------------------------------------------
+
+    // PRIVATE VALUES //
+    //--------------------------------------------------------------------------------------
+    //
+    private float m_fRotationX = 0.0f;
+    private float m_fRotationY = 0.0f;
+    //--------------------------------------------------------------------------------------
+
+    //--------------------------------------------------------------------------------------
+    // initialization.
+    //--------------------------------------------------------------------------------------
+    void Awake()
+    {
+        //
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    //--------------------------------------------------------------------------------------
+    // Update: Function that calls each frame to update game objects.
+    //--------------------------------------------------------------------------------------
+    void Update()
+    {
+        //
+        m_fRotationX += Input.GetAxis("Mouse Y") * m_fSensitivityX;
+        m_fRotationY += Input.GetAxis("Mouse X") * m_fSensitivityY;
+
+        //
+        m_fRotationX = Mathf.Clamp(m_fRotationX, m_fMinimumX, m_fMaximumX);
+
+        //
+        m_cCamera.transform.position = transform.position + offset;
+
+        //
+        transform.localEulerAngles = new Vector3(0, m_fRotationY, 0);
+
+        //
+        m_cCamera.transform.localEulerAngles = new Vector3(-m_fRotationX, m_fRotationY, 0);
+
+        //
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            //
+            Cursor.lockState = CursorLockMode.None;
+
+            //
+            Cursor.visible = true;
+        }
+    }
+
+    //--------------------------------------------------------------------------------------
+    // Movement: 
+    //--------------------------------------------------------------------------------------
+    void Movement()
+    {
+
+    }
+}
---
+++ Assets/Scripts/Player/Player.cs
@@ -0,0 +1,149 @@
+﻿// using, etc
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//--------------------------------------------------------------------------------------
+// Player object. Inheriting from MonoBehaviour. The main player class for player movement
+//--------------------------------------------------------------------------------------
+public class Player : MonoBehaviour
+{
+    // PUBLIC VALUES //
+    //--------------------------------------------------------------------------------------
+    // public float value for the walking speed.
+    public float m_fWalkSpeed;
+    //--------------------------------------------------------------------------------------
+
+    // PRIVATE VALUES //
+    //--------------------------------------------------------------------------------------
+    // private rigidbody
+    private Rigidbody m_rbRigidBody;
+
+    // private vector3 for the move direction
+    private Vector3 m_v3MoveDirection;
+
+
+
+
+
+
+
+    private bool m_bJump;
+    public int forceConst = 500;
+
+
+
+
+
+    //--------------------------------------------------------------------------------------
+
+    //--------------------------------------------------------------------------------------
+    // initialization.
+    //--------------------------------------------------------------------------------------
+    void Awake()
+    {
+        // Get the rigidbody component
+        m_rbRigidBody = GetComponent<Rigidbody>();		
+	}
+
+    //--------------------------------------------------------------------------------------
+    // Update: Function that calls each frame to update game objects.
+    //--------------------------------------------------------------------------------------
+    void Update()
+    {
+        // Get the horizontal and vertical axis
+        float fHor = Input.GetAxis("Horizontal");
+        float fVer = Input.GetAxis("Vertical");
+
+
+
+
+
+
+        //
+        float y = Camera.main.transform.eulerAngles.y;
+        float z = Camera.main.transform.eulerAngles.z;
+
+        //
+        Vector3 Dir = new Vector3(0, y, z);
+        transform.rotation = Quaternion.Euler(Dir);
+
+
+
+
+
+
+        // Apply axis values to the move direction variable
+        m_v3MoveDirection = (fHor * transform.right + fVer * transform.forward).normalized;
+
+
+
+
+
+
+
+
+        if (Input.GetKeyUp(KeyCode.Space) && IsGrounded())
+        {
+            m_bJump = true;
+        }
+    }
+
+    //--------------------------------------------------------------------------------------
+    // FixedUpdate: 
+    //--------------------------------------------------------------------------------------
+    void FixedUpdate()
+    {
+        // Run the movement function
+        Movement();
+
+
+        if (m_bJump)
+        {
+            m_bJump = false;
+            m_rbRigidBody.AddForce(0, forceConst, 0, ForceMode.Impulse);
+        }
+    }
+
+    //--------------------------------------------------------------------------------------
+    // Movement: 
+    //--------------------------------------------------------------------------------------
+    void Movement()
+    {
+
+
+
+
+        //Vector3 v3VelFix = new Vector3(0, m_rbRigidBody.velocity.y, 0);
+
+
+
+        // update the player volocity by move direction, walkspeed and deltatime.
+        m_rbRigidBody.AddForce(m_v3MoveDirection * m_fWalkSpeed, ForceMode.Acceleration);
+
+
+
+
+        //m_rbRigidBody.velocity += v3VelFix;
+    }
+
+    bool IsGrounded()
+    {
+        Ray rRay = new Ray(transform.position, Vector3.down);
+        RaycastHit rhHitInfo;
+
+        int layerMask = (LayerMask.GetMask("Ground"));
+
+        if (Physics.Raycast(rRay, out rhHitInfo, 1.0f, layerMask))
+        {
+            Debug.Log(rhHitInfo.collider.name);
+            return true;    
+        }
+
+        return false;
+    }
+}
+
+
+
+//https://www.mvcode.com/lessons/first-person-camera-and-controller-jamie
